d19b8f6521b920ca6a823c6e0ad865d312cd5638
Douglas Lima <douglas.lima.queiroz@gmail.com>
2024-04-29 00:10:39 -0300

    fix: vercel

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,18 +5,17 @@ import { buscar } from "../../services/Service";
 import CardProduto from "../../components/produtos/cardProduto/CardProduto";
 import { Link } from "react-router-dom";
 import Produto from "../../models/Produto";
+
 function Home() {
   const [produtos, setProdutos] = useState<Produto[]>([]);
+
 
   useEffect(() => {
     async function fetchData() {
       try {
-        // Atribua o resultado de buscar a uma variável para garantir o tipo correto
-        const response = await buscar("/produtos", setProdutos, {});
-        // Verifique se response é um array antes de aplicar filter
+        const response = await buscar("/produtos", setProdutos, {}); // Verifique se buscar retorna um array de produtos
         if (Array.isArray(response)) {
-          const produtosLimitados = response.filter((_, index) => index < 8);
-          setProdutos(produtosLimitados);
+          setProdutos(response); // Atualize 'produtos' diretamente com os dados da API
         }
       } catch (error) {
         console.error("Erro ao buscar produtos:", error);
@@ -35,15 +34,13 @@ function Home() {
           <div className="acessar-produtos w-">
             <h1 className=" text-4xl font-semibold text-zinc-800">Produtos</h1>
             <Link to="/produtos">
-              <p className="text-2xl font-normal text-gray-900 hover:text-green-500">
-                Ver mais
-              </p>
+              <p className="text-2xl font-normal text-gray-900 hover:text-green-500">Ver mais</p>
             </Link>
           </div>
           {/* Exibe os cards de produto */}
           <div className="flex gap-3 flex-wrap py-8 justify-center items-center produtos">
-            {(produtos as { id: string }[]).slice(0, 8).map((produto) => (
-              <CardProduto key={produto.id} produto={produto} />
+            {produtos.slice(0, 8).map((produto) => (
+              <CardProduto key={String(produto.id)} produto={produto} />
             ))}
           </div>
         </div>
