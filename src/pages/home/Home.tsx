639d0be167723d3a472b110fa27b400802d28d80
Douglas Lima <douglas.lima.queiroz@gmail.com>
2024-04-29 00:05:25 -0300

    fix: vercel

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -4,16 +4,20 @@ import { useEffect, useState } from "react";
 import { buscar } from "../../services/Service";
 import CardProduto from "../../components/produtos/cardProduto/CardProduto";
 import { Link } from "react-router-dom";
+import Produto from "../../models/Produto";
 function Home() {
-  const [produtos, setProdutos] = useState([]);
+  const [produtos, setProdutos] = useState<Produto[]>([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
+        // Atribua o resultado de buscar a uma variável para garantir o tipo correto
         const response = await buscar("/produtos", setProdutos, {});
-        // Limita a exibição a no máximo 8 produtos
-        const produtosLimitados = response.slice(0, 8);
-        setProdutos(produtosLimitados);
+        // Verifique se response é um array antes de aplicar filter
+        if (Array.isArray(response)) {
+          const produtosLimitados = response.filter((_, index) => index < 8);
+          setProdutos(produtosLimitados);
+        }
       } catch (error) {
         console.error("Erro ao buscar produtos:", error);
       }
@@ -31,7 +35,9 @@ function Home() {
           <div className="acessar-produtos w-">
             <h1 className=" text-4xl font-semibold text-zinc-800">Produtos</h1>
             <Link to="/produtos">
-            <p className="text-2xl font-normal text-gray-900 hover:text-green-500" >Ver mais</p>
+              <p className="text-2xl font-normal text-gray-900 hover:text-green-500">
+                Ver mais
+              </p>
             </Link>
           </div>
           {/* Exibe os cards de produto */}
